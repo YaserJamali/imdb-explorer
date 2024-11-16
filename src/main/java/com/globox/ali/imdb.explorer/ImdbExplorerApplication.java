50c63ed8907305ae24816e051f605be2587aa9a5
YaserJamali <yaser.jamali@outlook.com>
2024-11-16 16:17:07 +0330

    FEAT: this the first step of this project

---
+++ src/main/java/com/globox/ali/imdb.explorer/ImdbExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.globox.ali.imdb.explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/globox/ali/ali_imdb_explorer/ImdbExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globox.ali.ali_imdb_explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
